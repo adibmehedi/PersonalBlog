1df9e479e68a91f92fa0ec1fb862f2cb9cf65763
Adib Mehedi <adibmehedi@gmail.com>
2017-07-20 15:51:46 +0545

    Initial Setup

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function post(){
+        return view('post');
+    }
+}
